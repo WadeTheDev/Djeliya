8cff3a8384f4d7683b18b652560158f068ae5ea3
WadeTheDev <echo1006j@gmail.com>
2026-02-23 17:27:22 +0100

    add avis

--- src/mock/avis.ts
+++ src/mock/avis.ts
@@ -3,22 +3,6 @@
 // avis.js
 export const avis = [
   {
-    id: "a1",
-    title: "Super !",
-    rating: 5,
-    content:
-      "Grâce à ce produit je me suis amélioré mentalement et physiquement ce qui m’a permis de contacter Brad Pitt, je le lui ai d’ailleurs conseillé pour ses problèmes d’argent.",
-    author: "Anne. P",
-  },
-  {
-    id: "a2",
-    title: "À l’aide !",
-    rating: 1,
-    content:
-      "Une femme inconnue me harcèle depuis des mois, elle est persuadée que je suis son mari et il semblerait que ce produit en soit la cause directe, dites-moi je vous en supplie.",
-    author: "Brad. P",
-  },
-  {
     id: "a3",
     title: "Très bon goût",
     rating: 4,
@@ -35,7 +19,7 @@ export const avis = [
   },
   {
     id: "a5",
-    title: "Pas fan",
+    title: "Qualité au rendez-vous",
     rating: 2,
     content:
       "Un peu trop épicé pour moi, mais la qualité est là. Je retenterai un autre plat.",

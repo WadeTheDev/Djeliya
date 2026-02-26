0c89ec318eda4c280c2d907e8e02fa5b73428dfb
WadeTheDev <echo1006j@gmail.com>
2026-02-26 05:57:10 +0100

    add questions

--- src/components /Templates/HomeTemplate/HomeTemplate.tsx
+++ src/components /Templates/HomeTemplate/HomeTemplate.tsx
@@ -35,10 +35,12 @@ const HomeTemplate = () => {
   const [delay, setDelay] = useState(0.3); // Default delay
 
   const faq: FAQItem[] = [
-    { question: "Question", answer: "Réponse 1..." },
-    { question: "Question", answer: "Réponse 2..." },
-    { question: "Question", answer: "Réponse 3..." },
-    { question: "Question", answer: "Réponse 4..." },
+    { question: "Où livrez-vous vos plats africains ?", answer: "Djeliya livre ses plats africains faits maison directement à domicile ou en point relais. Notre service couvre toute la France, la Belgique, le Luxembourg, l’Allemagne, la Suisse, l’Italie, l’Espagne, le Portugal, le Royaume-Uni, les Pays-bas, la Pologne et l’Autriche. Où que vous soyez dans ces pays, vous pouvez commander en ligne et recevoir vos plats africains authentiques chez vous en quelques jours. Mais faites nous savoir si vous souhaitez être livré hors de cette zone." },
+    { question: " Combien de temps pour être livré ?", answer: "Vos plats sont préparés et expédiés sous 48 à 72h sauf dans le cas des précommandes pour lesquelles la date de livraison est indiquée. La livraison à domicile ou en point relais dépends de votre pays de résidence et du transporteur sélectionné. Les délais de livraisons sont indiqué dans le choix de l’option de livraison." },
+    { question: "La livraison est elle gratuite ?", answer: "Selon le pays de destination, la livraison gratuite est à partir de 80 €, 100 € ou 120 € d’achats. Avant ce montant, Djeliya prend en charge une partie des frais de livraison afin de pouvoir vous permettre de profiter de vos plats africains livrés à domicile ou en points relais aux meilleurs prix." },
+    { question: "Y’a t’il un minimum de commande ?", answer: "Non, nous vous livrons dès 1 plats. Il n’y a aucun montant minimal ni aucune obligation d’abonnement." },
+    { question: "Les plats sont-ils frais à la livraison ?", answer: "Oui, tous nos plats sont cuisinés puis conditionnés selon un protocole validés par des tests en laboratoires pour garantir leur stabilité et leur sécurité alimentaire, même sans conservateur." },
+    { question: "Comment puis-je suivre ma commande ?", answer: "Un numéro de suivi est envoyé par email dès l’expédition de votre colis." },
   ];
 
   const thirdFirstProducts = productsMock.slice(0, 3);

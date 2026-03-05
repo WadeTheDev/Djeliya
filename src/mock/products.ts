9bcd60ebc10ec1f3a8700108806ab0fd10aafb07
WadeTheDev <echo1006j@gmail.com>
2026-03-05 05:38:02 +0100

    add mocks

--- src/mock/products.ts
+++ src/mock/products.ts
@@ -12,9 +12,9 @@ export const productsMock: Product[] = [
     quote:
       "Un plat emblématique aux saveurs authentiques d’Afrique de l’Ouest.",
     description:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.      ",
+      "Plongez dans les saveurs profondes du Sénégal avec notre thieb, le plat national emblématique. À base de brisure de riz parfumé longuement mijoté dans une sauce tomate, ce mets est accompagné de poulet mariné, et de légumes fondants. Chaque bouchée vous transporte au cœur de l’Afrique de l’Ouest, entre générosité, chaleur et authenticité.",
     shortDescription:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. ",
+      "En raison d’une erreur d’inscription sur le packaging où il est inscrit « Thieboudienne au poulet » au lieu de « Thieb au poulet », une réduction de 10% vous est accordée avec le code promo THIEB (offre non cumulable).",
     price: 9.9,
     images: {
       packaging: {
@@ -73,8 +73,9 @@ export const productsMock: Product[] = [
     rating: {value: 4.8, count: 94},
     quote: "La douceur de l’arachide, la puissance du bœuf.",
     description:
-      "Un ragoût onctueux à base de pâte d’arachide, de bœuf fondant et de légumes mijotés lentement.",
-    shortDescription: "Bœuf fondant et sauce arachide onctueuse.",
+      "Mafé au Bœuf accompagné de Riz : Découvrez la richesse du Mafé, un classique de la cuisine ouest-africaine. Ce plat réconfortant associe de tendres morceaux de bœuf mijotés dans une onctueuse sauce à base de pâte d’arachide, subtilement relevée d’épices et de tomates. Servi avec du riz blanc parfumé, le mafé au bœuf séduit par sa douceur, sa profondeur de goût et sa capacité à rassembler autour de la table.",
+    shortDescription:
+      "Bœuf fondant et sauce arachide onctueuse, un classique ouest-africain refaçonné pour un goût authentique et accessible à tous.",
     price: 10.9,
     images: {
       packaging: {
@@ -114,16 +115,18 @@ export const productsMock: Product[] = [
   },
 
   {
-    id: "yassa-poulet",
-    slug: "yassa-au-poulet",
+    id: "foutou-graine",
+    slug: "foutou-sauce-graine",
     isNew: false,
-    name: "Yassa au poulet",
+    name: "Foutou sauce graine",
     rating: {value: 4.5, count: 76},
-    quote: "L’acidité parfaite, entre citron et oignons confits.",
+    quote:
+      "La douceur du foutou, la richesse de la sauce graine, un voyage culinaire en Afrique de l’Ouest.",
     description:
-      "Poulet mariné au citron et aux oignons, mijoté lentement pour une explosion de saveurs.",
-    shortDescription: "Poulet citronné et oignons fondants.",
-    price: 9.5,
+      "Foutou Sauce Graine au Bœuf et Poulet : Un délicieux plat d’Afrique de l’Ouest composé de foutou moelleux, mélange de manioc et de banane plantain, accompagné d’une sauce graine onctueuse enrichie de morceaux de bœuf et de poulet mijotés lentement. Un voyage culinaire savoureux, prêt en quelques minutes.",
+    shortDescription:
+      "Un délicieux plat d’Afrique de l’Ouest composé de foutou moelleux, mélange de manioc et de banane plantain, accompagné d’une sauce graine onctueuse enrichie de morceaux de bœuf et de poulet mijotés lentement.",
+    price: 14.99,
     images: {
       packaging: {
         src: "/img/Product3.png",
@@ -159,15 +162,16 @@ export const productsMock: Product[] = [
   },
 
   {
-    id: "thieb-veggie",
-    slug: "thieb-vegetarien",
+    id: "poulet-yassa",
+    slug: "poulet-yassa",
     isNew: false,
-    name: "Thiéb végétarien",
+    name: "Poulet Yassa – Riz",
     rating: {value: 4.3, count: 52},
-    quote: "Toute la richesse du thiéb, version végétale.",
+    quote: "un poulet yassa généreux pour découvrir les saveurs du Sénégal.",
     description:
-      "Un thiéb généreux composé uniquement de légumes et de riz parfumé.",
-    shortDescription: "Riz parfumé et légumes mijotés.",
+      "Plongez dans l’authenticité de la cuisine africaine avec notre Poulet Yassa au Riz, un plat emblématique aux saveurs intenses et équilibrées. Préparé avec des ingrédients soigneusement sélectionnés, ce délicieux plat marie la tendreté du poulet à une sauce onctueuse aux oignons et au citron, le tout accompagné de riz parfumé.",
+    shortDescription:
+      "plongée dans les saveurs du Sénégal avec notre Poulet Yassa au Riz, un plat emblématique aux saveurs intenses et équilibrées.",
     price: 8.9,
     images: {
       packaging: {
@@ -204,15 +208,17 @@ export const productsMock: Product[] = [
   },
 
   {
-    id: "poulet-dg",
-    slug: "poulet-dg",
+    id: "box-abidjanaise",
+    slug: "box-abidjanaise",
     isNew: false,
-    name: "Poulet DG",
+    name: "Box L’Abidjanaise: Foutou – Mafé – Attiéké",
     rating: {value: 4.7, count: 61},
-    quote: "Entre tradition et street food africaine.",
+    quote:
+      "un mixte de saveurs d’Abidjan pour une expérience culinaire complète.",
     description:
-      "Poulet sauté accompagné de bananes plantain et de légumes croquants.",
-    shortDescription: "Poulet sauté et plantain doré.",
+      "Une box gourmande qui vous transporte au cœur d’Abidjan avec un mélange de saveurs authentiques. Découvrez le foutou, une purée de banane plantain onctueuse, accompagnée d’un mafé au bœuf riche en goût et d’un attiéké léger et parfumé. Chaque bouchée est une invitation à un voyage culinaire inoubliable à travers les rues animées d’Abidjan.",
+    shortDescription:
+      "En cinq minutes de préparation, plongez dans les saveurs d’Abidjan avec notre box gourmande.",
     price: 11.5,
     images: {
       packaging: {
